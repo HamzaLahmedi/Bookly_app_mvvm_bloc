148f5c5a4beb0302edc0904e67499c98ceb3230f
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-02-18 17:19:50 +0100

    refactor code and navigate to home

--- bookly_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ bookly_app/lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,7 +1,10 @@
+import 'package:bookly_app/constant.dart';
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/features/home/presentation/views/home_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/widgets/sliding_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
 
 class SplashViewBody extends StatefulWidget {
   const SplashViewBody({super.key});
@@ -17,17 +20,11 @@ class _SplashViewBodyState extends State<SplashViewBody>
   @override
   void initState() {
     super.initState();
-    animationController = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 2),
-    );
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
-            .animate(animationController);
-
-    //activate animation
-    animationController.forward();
+    initSlidingAnimation();
+    navigateToHome();
   }
+
+  
 
   @override
   void dispose() {
@@ -48,4 +45,34 @@ class _SplashViewBodyState extends State<SplashViewBody>
       ],
     );
   }
+
+  void initSlidingAnimation() {
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    );
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 2), end: Offset.zero)
+            .animate(animationController);
+
+    //activate animation
+    animationController.forward();
+  }
+
+
+
+
+
+  void navigateToHome() {
+    Future.delayed(const Duration(seconds: 2), () {
+      Get.to(
+        const HomeView(),
+        transition: Transition.fade,
+        duration: kTransitionDuration,
+      );
+    });
+  }
+
+
+  
 }
