8bbf0638df2d0aedcf7ab2321aa4978610b9a01e
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-02-24 15:16:44 +0100

    create FetchFeaturedBooksUseCase

--- bookly_app/lib/features/home/domain/repos/home_repos.dart
+++ bookly_app/lib/features/home/domain/repos/home_repos.dart
@@ -1,7 +1,8 @@
+import 'package:bookly_app/core/errors/failure.dart';
 import 'package:bookly_app/features/home/domain/entities/book_entity.dart';
+import 'package:dartz/dartz.dart';
 
-abstract class HomeRepo{
-  Future<List<BookEntity>> fetchFeaturedBooks ();
-    Future<List<BookEntity>> fetchNewestBooks ();
-
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks();
+  Future<Either<Failure, List<BookEntity>>> fetchNewestBooks();
 }
