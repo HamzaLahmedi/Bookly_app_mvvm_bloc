edf4205d31fdbeb96f07f418d6cfea850d11331d
HamzaLahmedi <hamzalahmedi07@gmail.com>
2024-02-26 15:08:43 +0100

    cached network image

--- bookly_app/lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ bookly_app/lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,26 +1,24 @@
 import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/widgets/custom_loading_indicator.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookItem extends StatelessWidget {
   const CustomBookItem({super.key, required this.imageUrl});
-final String imageUrl;
+  final String imageUrl;
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 4),
-      child: AspectRatio(
-        aspectRatio: 2.7 / 4,
-        child: Container(
-          decoration:  BoxDecoration(
-            borderRadius:const BorderRadius.all(
-              Radius.circular(16),
-            ),
-            
-            image: DecorationImage(
-                image: NetworkImage(
-                  imageUrl,
-                ),
-                fit: BoxFit.fill),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(16),
+        child: AspectRatio(
+          aspectRatio: 2.7 / 4,
+          child: CachedNetworkImage(
+            imageUrl: imageUrl,
+            fit: BoxFit.fill,
+            placeholder: (context, url) => const CustomLoadingIndicator(),
+            errorWidget: (context, url, error) =>const Icon(Icons.error),
           ),
         ),
       ),
